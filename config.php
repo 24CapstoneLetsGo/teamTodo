4b2697466d9daca162ff99a92e8f9bf7ece02b77
zox2m <zox2mm@gmail.com>
2024-06-09 12:57:33 +0000

    web 표시, sql 초기 설정 진행중 -> init 실행하도록

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost"; // 데이터베이스 서버 주소
+$servername = "db"; // 데이터베이스 서버 주소
 $username = "lego";             // 데이터베이스 사용자 이름
 $password = "lego";                 // 데이터베이스 비밀번호
 $dbname = "team_todo";          // 데이터베이스 이름
@@ -10,6 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // 연결 확인
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
+}   
 ?>
 
